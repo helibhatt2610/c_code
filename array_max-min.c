bc87484e088295e74208449d7c23bcfeb45b1681
helibhatt2610 <helibhatt026@gmail.com>
2024-02-12 20:59:08 +0530

    min ane max maate

---
+++ array_max-min.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+	int n=0;
+	int a[5]={1,7,10,9,5};
+	//ahi number 1 to 10 ni vachhe lidhelo chee
+	while(n<5)
+	{
+		printf("10-%d=%d\n",a[n],10-a[n]);
+		
+		n++;
+	}
+}
