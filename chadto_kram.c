96d2dffd6378afc04365be41c5dc3f3d739f6b79
helibhatt2610 <helibhatt026@gmail.com>
2024-02-13 00:16:14 +0530

    if & else if

--- chadto_kram.c
+++ chadto_kram.c
@@ -3,28 +3,12 @@
 void main()
 {
 	int a[5]={1,9,5,3,8};
+	int b[5]={10,10,10,10,10};
 	int n=0;
-	if(a[0]>a[1]>a[2]>a[3]>a[4])
+	while(n>5)
 	{
-		printf("so max is %d",a[0]);
-	}
-	else if(a[1]>a[2]>a[3]>a[4]>a[5])
-         {
-                 printf("so max is %d",a[1]);
-         }
-	else if(a[2]>a[3]>a[4]>a[5]>a[1])
-         {
-                 printf("so max is %d",a[2]);
-         }
-	else if(a[3]>a[4]>a[5]>a[1]>a[2])
-         {
-                 printf("so max is %d",a[3]);
-         }
-	else if(a[4]>a[5]>a[1]>a[2]>a[3])
-         {
-                 printf("so max is %d",a[4]);
-         }
-
-		
 	
+		printf("%d",b[n]-a[n]);
+		n++;
+	}
 }
