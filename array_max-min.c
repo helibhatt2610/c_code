a98034ed2d628c87959cae7819766eea37584b57
helibhatt2610 <helibhatt026@gmail.com>
2024-02-12 23:15:58 +0530

    else if thi try

--- array_max-min.c
+++ array_max-min.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+ #include<stdio.h>
 
 void main()
 {
@@ -10,7 +10,57 @@ void main()
 		int p=10-a[n];
 		printf("10-%d=%d   ",a[n],p);
 		printf("1<=%d<=10\n",p);
-		
+        if(p==0)
+        {
+                 printf("%d suathi max. no che\n",a[n]);
+        }
+	else if(p==10)
+	{
+		printf("%d suthi min. no che\n",a[n]);
+	}
+	if(p==1)
+         {
+                  printf("%d suathi max. no che\n",a[n]);
+         }
+         else if(p==9)
+         {
+                 printf("%d suthi min. no che\n",a[n]);
+         }
+	if(p==2)
+         {
+                  printf("%d suathi max. no che\n",a[n]);
+         }
+         else if(p==8)
+         {
+                 printf("%d suthi min. no che\n",a[n]);
+         }
+	if(p==3)
+         {
+                  printf("%d suathi max. no che\n",a[n]);
+         }
+         else if(p==7)
+         {
+                 printf("%d suthi min. no che\n",a[n]);
+         }
+	if(p==4)
+         {
+                  printf("%d suathi max. no che\n",a[n]);
+         }
+         else if(p==6)
+         {
+                 printf("%d suthi min. no che\n",a[n]);
+         }
+	if(p==5)
+         {
+                   printf("%d suathi max. no che\n",a[n]);
+         }
+         else if(p==5)
+         {
+                 printf("%d suthi min. no che\n",a[n]);
+         }
+
+	
+
 		n++;
 	}
 }
