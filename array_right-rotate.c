74090455924938d88a605980db59e36deeab4449
helibhatt2610 <helibhatt026@gmail.com>
2024-02-13 00:44:42 +0530

    rotate karava maate

---
+++ array_right-rotate.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+	int a[5]={1,2,3,4,5};
+	int n=0;
+	while(n<10)
+	{
+		if(n>5)
+		{
+			printf("no:%d\n",a[n]);
+			
+		}
+		else
+		{
+			printf("no:%d\n",a[n-5]);
+			
+		}
+		n++;
+	}
+
+	
+}
