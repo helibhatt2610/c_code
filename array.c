0663ec4082482abc07dd71013793cd9817fc47f3
helibhatt2610 <helibhatt026@gmail.com>
2024-02-12 11:51:04 +0530

    number print karava mate

--- array.c
+++ array.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 void main()
-{
-	printf("Hello,world\n");
+{	
+	int n=0;
+	int a[5]={5,6,7,8,9};
+	while(n<=4)
+	{
+		printf("%d\n",a[n]);
+		n++;
+		//printf("Hello,world\n");
+	}
 }
