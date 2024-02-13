0bd3023d4d5173af71935e520ad914a963cc2e3a
helibhatt2610 <helibhatt026@gmail.com>
2024-02-13 23:14:18 +0530

    undhu print karva mate

---
+++ String_undhu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+ void main()
+ {
+         char str[]="computer";
+         int n=8;
+         while(0<=n)
+         {
+         printf("%c",str[n]);
+         n--;
+         }
+ 
+ }
+
