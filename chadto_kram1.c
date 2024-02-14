f43c24f7e0907a64e9afcb0de8c72cf30c2e6e98
helibhatt2610 <helibhatt026@gmail.com>
2024-02-14 23:51:02 +0530

    chadto kram apva maate i=0 lidhu

---
+++ chadto_kram1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+	int a[5]={10,15,20,7,2};
+	int n=0;	
+	int i=0;
+	while(i<100)
+	{
+		if(i==a[n])
+		{
+			System.out.println("%d",a[n]);
+			i++;
+		}
+		else
+		{
+			System.out.println("");
+			i++;
+		}
+		
+	}
+}
