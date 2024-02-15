54e718f6f0c5846b4b0ede6c9c36e96b6f4edd91
helibhatt2610 <helibhatt026@gmail.com>
2024-02-15 23:56:32 +0530

    chadto kram ma sankhya

--- chadto_kram1.c
+++ chadto_kram1.c
@@ -5,9 +5,9 @@ void main()
 	int a[5]={10,15,20,7,2};
 	int n=0;	
 	int i=0;
-	while(n<5)
+	while(i<21)
 	{
-		while(i<100)
+		while(n<5)
 		{
 			if(i==a[n])
 			{
@@ -19,8 +19,8 @@ void main()
 				printf("");
 			
 			}
-			i++;
-		}int i=0;
-		n++;
+			n++;
+		}n=0;
+		i++;
 	}
 }
