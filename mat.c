f5ed7e5879645fcc967d658721d2ce9237f2898f
helibhatt2610 <helibhatt026@gmail.com>
2024-02-20 23:38:37 +0530

    mat print karva maate

--- mat.c
+++ mat.c
@@ -16,14 +16,14 @@ void main()
 	a[2][1]=0;
 	a[2][2]=1;
 	
-	printf("%d\n",a[0][0]);
-	printf("%d\n",a[0][1]);
-	printf("%d\n",a[0][2]);
-	printf("%d\n",a[1][0]);
-	printf("%d\n",a[1][1]);
-	printf("%d\n",a[1][2]);
-	printf("%d\n",a[2][0]);
-	printf("%d\n",a[2][1]);
-	printf("%d\n",a[2][2]);
+	printf(" %d ",a[0][0]);
+	printf(" %d ",a[0][1]);
+	printf(" %d\n",a[0][2]);
+	printf(" %d ",a[1][0]);
+	printf(" %d ",a[1][1]);
+	printf(" %d\n",a[1][2]);
+	printf(" %d ",a[2][0]);
+	printf(" %d ",a[2][1]);
+	printf(" %d\n",a[2][2]);
 
 }
