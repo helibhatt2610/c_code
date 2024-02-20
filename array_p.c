8913a81f344d5c3f0fecb51212bbeebc5947db51
helibhatt2610 <helibhatt026@gmail.com>
2024-02-20 23:16:08 +0530

    pointer test karva maate

--- array_p.c
+++ array_p.c
@@ -10,6 +10,11 @@ void main()
 	printf("%p\n",p);
 	printf("%p\n",&p);
 	printf("%p\n",&b);
+	printf("value of a[0]%p\n",&a[0]);
+	printf("value of a[1]%p\n",&a[1]);
+	printf("value of a[2]%p\n",&a[2]);
+	printf("value of a[3]%p\n",&a[3]);
+	printf("value of a[4]%p\n",&a[4]);
 	
 	
 }
