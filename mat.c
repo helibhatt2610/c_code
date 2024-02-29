c827537b56087373e8a6044f5d3cc4fc7bfd878d
helibhatt2610 <helibhatt026@gmail.com>
2024-02-29 23:00:57 +0530

    matrix sum

--- mat.c
+++ mat.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+struct matrix
+{
+	int a[3][3];
+	int b[3][3];
+};
  
- void main()
- {
-         int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-         int b[3][3] = {{10,11,12},{13,14,15},{16,17,18}};
-         for(int i=0;i<3;i++)
-         {
-                 for(int j=0;j<3;j++)
-                 {
-                         printf(" %d ",a[i][j]);
-                 }
-                 printf("\n");
-         }
-         for(int i=0;i<3;i++)
-         {
-                 for(int j=0;j<3;j++)
-                 {
-                         printf(" %d ",b[i][j]);
-                 }
-                 printf("\n");
-         }
- 
- }   
+void main()
+{
+        int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+        int b[3][3] = {{10,11,12},{13,14,15},{16,17,18}};
+        for(int i=0;i<3;i++)
+        {
+                for(int j=0;j<3;j++)
+                {
+                        printf(" %d ",a[i][j]);
+                }
+                printf("\n");
+        }
+        for(int i=0;i<3;i++)
+        {
+                for(int j=0;j<3;j++)
+                {
+                        printf(" %d ",b[i][j]);
+                }
+                printf("\n");
+        } 
+}   
 
