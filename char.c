8a189c882320ec83e0e8b680e3e92201ff32189b
helibhatt2610 <helibhatt026@gmail.com>
2024-03-29 11:36:06 +0530

    char print karva maate

--- char.c
+++ char.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
-
+struct print
+{
+	char st[100];
+	int a;
+};
 void main()
 {
-	 char str[100]="student";
-	printf("%c\n",str[2]);
-	printf("%s\n",str);
+	struct print p;
+	p.st[100] = "student";
+	
+//	printf("%c\n",str[2]);
+	printf("%s\n",st);
 }
