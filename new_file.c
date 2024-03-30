006966b3983a254bdc493d1d398ea55beaaa3dc9
helibhatt2610 <helibhatt026@gmail.com>
2024-03-30 12:25:04 +0530

    file open kari ne lakhva

--- new_file.c
+++ new_file.c
@@ -2,9 +2,16 @@
 
 void main()
 {
-	FILE *name; 
-	name = fopen("new.txt","w");
+	FILE *name;
+//	 name = fopen("new.txt","w"); 
+	name = fopen("new.txt","r");
+	char str[100];
 	
-	fprintf(name,"some text");
+	fprintf(name,"some text\nhello student\n");
+	fgets(str,100,name);
+	printf("   %s\n",str);
+	fgets(str,100,name);
+	printf("   %s\n",str);
+
 	fclose(name);
 }
